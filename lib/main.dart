115e82d44b0fc10f8e728cbe6aa65baa0a49d106
Arshia Moghimi <arshiamoghimi98@gmail.com>
2021-07-25 20:15:10 +0430

    fonts moved under assets directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:copark/settings/parse.dart';
+import 'package:copark/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:parse_server_sdk_flutter/generated/i18n.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:flutter/services.dart';
+import 'dashboard_screen.dart';
+import 'login_screen.dart';
+import 'transition_route_observer.dart';
+import 'dashboard_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+      SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -16,22 +28,10 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> initData() async {
-    // Initialize repository
-    // await initRepository();
-    // await initCoreStore();
-
     // Initialize parse
     await Parse().initialize(keyParseApplicationId, keyParseServerUrl,
-        // clientKey: keyParseClientKey,
         debug: true,
         coreStore: await CoreStoreSharedPrefsImp.getInstance());
-
-    //parse serve with secure store and desktop support
-
-    //    Parse().initialize(keyParseApplicationId, keyParseServerUrl,
-    //        clientKey: keyParseClientKey,
-    //        debug: true,
-    //        coreStore: CoreStoreSharedPrefsImp.getInstance());
 
     // Check server is healthy and live - Debug is on in this instance so check logs for result
     final ParseResponse response = await Parse().healthCheck();
@@ -44,53 +44,123 @@ class _MyAppState extends State<MyApp> {
       print(text);
     }
 
-    final ParseUser? user1 = await ParseUser.currentUser();
-    print('User: $user1');
-
-
-
-    ParseUser user = ParseUser('ali2', 'password', 'alirtofighim2@gmail.com');
-    // user.verificationEmailRequest()
-    //
-    // // /// Sign-up
-    // // ParseResponse userResponse = await user.signUp();
-    // // if (userResponse.success) {
-    // //   user = userResponse.result;
-    // // }
-    //
-    // ParseResponse userResponse = await user.login();
-    //
-    // print(user);
+    UserModel.user = await ParseUser.currentUser();
   }
 
   @override
   Widget build(BuildContext context) {
-    // loc = AppLocalizations.of(context);
-
-    return MaterialApp(
-      title: 'کوپارک',
-      localizationsDelegates: const [
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale("fa", "IR"),
-      ],
-      locale: const Locale("fa", "IR"),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Vazir',
-      ),
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const MyHomePage(title: 'کوپارک'),
-        '/2': (context) => const MyHomePage(title: 'کوپارک 2'),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        // '/second': (context) => const SecondScreen(),
-      },
-    );
+    if (UserModel.user == null) {
+      return MaterialApp(
+        title: 'ورود',
+        localizationsDelegates: const [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale("fa", "IR"),
+        ],
+        locale: const Locale("fa", "IR"),
+        theme: ThemeData(
+          // brightness: Brightness.dark,
+          primarySwatch: Colors.deepPurple,
+          accentColor: Colors.orange,
+          textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Colors.orange),
+          // fontFamily: 'SourceSansPro',
+          textTheme: TextTheme(
+            headline3: const TextStyle(
+              fontFamily: 'Vazir',
+              fontSize: 45.0,
+              // fontWeight: FontWeight.w400,
+              color: Colors.orange,
+            ),
+            button: const TextStyle(
+              // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+              fontFamily: 'Vazir',
+            ),
+            caption: TextStyle(
+              fontFamily: 'Vazir',
+              fontSize: 12.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.deepPurple[300],
+            ),
+            headline1: TextStyle(fontFamily: 'Vazir'),
+            headline2: TextStyle(fontFamily: 'Vazir'),
+            headline4: TextStyle(fontFamily: 'Vazir'),
+            headline5: TextStyle(fontFamily: 'Vazir'),
+            headline6: TextStyle(fontFamily: 'Vazir'),
+            subtitle1: TextStyle(fontFamily: 'Vazir'),
+            bodyText1: TextStyle(fontFamily: 'Vazir'),
+            bodyText2: TextStyle(fontFamily: 'Vazir'),
+            subtitle2: TextStyle(fontFamily: 'Vazir'),
+            overline: TextStyle(fontFamily: 'Vazir'),
+          ),
+        ),
+        navigatorObservers: [TransitionRouteObserver()],
+        initialRoute: LoginScreen.routeName,
+        routes: {
+          LoginScreen.routeName: (context) => LoginScreen(),
+          DashboardScreen.routeName: (context) => DashboardScreen(),
+        },
+      );
+    }
+    else {
+      return MaterialApp(
+        title: 'کوپارک',
+        localizationsDelegates: const [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale("fa", "IR"),
+        ],
+        locale: const Locale("fa", "IR"),
+        theme: ThemeData(
+          // brightness: Brightness.dark,
+          primarySwatch: Colors.deepPurple,
+          accentColor: Colors.orange,
+          textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Colors.orange),
+          // fontFamily: 'SourceSansPro',
+          textTheme: TextTheme(
+            headline3: const TextStyle(
+              fontFamily: 'Vazir',
+              fontSize: 45.0,
+              // fontWeight: FontWeight.w400,
+              color: Colors.orange,
+            ),
+            button: const TextStyle(
+              // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+              fontFamily: 'Vazir',
+            ),
+            caption: TextStyle(
+              fontFamily: 'Vazir',
+              fontSize: 12.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.deepPurple[300],
+            ),
+            headline1: TextStyle(fontFamily: 'Vazir'),
+            headline2: TextStyle(fontFamily: 'Vazir'),
+            headline4: TextStyle(fontFamily: 'Vazir'),
+            headline5: TextStyle(fontFamily: 'Vazir'),
+            headline6: TextStyle(fontFamily: 'Vazir'),
+            subtitle1: TextStyle(fontFamily: 'Vazir'),
+            bodyText1: TextStyle(fontFamily: 'Vazir'),
+            bodyText2: TextStyle(fontFamily: 'Vazir'),
+            subtitle2: TextStyle(fontFamily: 'Vazir'),
+            overline: TextStyle(fontFamily: 'Vazir'),
+          ),
+        ),
+        navigatorObservers: [TransitionRouteObserver()],
+        initialRoute: DashboardScreen.routeName,
+        routes: {
+          LoginScreen.routeName: (context) => LoginScreen(),
+          DashboardScreen.routeName: (context) => DashboardScreen(),
+        },
+      );
+    }
   }
 }
 
@@ -100,90 +170,5 @@ class MyApp extends StatefulWidget {
   @override
   State createState() {
     return _MyAppState();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'شما اینقد کلیک کردی',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }

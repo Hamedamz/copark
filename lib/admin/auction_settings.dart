1cc1e7ff5ace0aabed25e4ea9f6bac6506f862b9
Hamed AMZ <amzhamed@gmail.com>
2021-07-27 04:58:01 +0430

    feat: add edit auction form

--- lib/admin/auction_settings.dart
+++ lib/admin/auction_settings.dart
@@ -17,13 +17,93 @@ class _AuctionSettingsState extends State<AuctionSettings> {
   @override
   Widget build(BuildContext context) {
     prepareSettings();
-    return Center(
-          child: Column(children: [
-            Text(
-                'حداقل قیمت: ${settings == null ? 0 : settings!.minimumPrice}   ریال'),
-            Text('تعداد پارکینگ‌ها: ${settings == null ? 0 : settings!.parkCount}')
-          ]),
-        );
+
+    var _minimumPrice = settings == null ? 0 : settings?.minimumPrice;
+    var _parkCount = settings == null ? 0 : settings?.parkCount;
+    var _auctionInprogressDays =
+        settings == null ? 0 : settings?.auctionInprogressDays;
+    var _auctionNewDays = settings == null ? 0 : settings?.auctionNewDays;
+    // return Center(
+    //       child: Column(children: [
+    //         Text(
+    //             'حداقل قیمت: ${settings == null ? 0 : settings!.minimumPrice}   ریال'),
+    //         Text('تعداد پارکینگ‌ها: ${settings == null ? 0 : settings!.parkCount}')
+    //       ]),
+    //     );
+    return Container(
+      child: Scrollbar(
+        child: SingleChildScrollView(
+            padding: new EdgeInsets.all(20.0),
+            child: Column(children: [
+              ...[
+                TextFormField(
+                  restorationId: 'min_price',
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  initialValue: _minimumPrice.toString(),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'حداقل قیمت',
+                      suffixText: 'تومان'),
+                ),
+                TextFormField(
+                  restorationId: 'parking_count',
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  initialValue: _parkCount.toString(),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'تعداد پارکینگ',
+                  ),
+                ),
+                TextFormField(
+                  restorationId: 'progress_days',
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  initialValue: _auctionInprogressDays.toString(),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'طول مزایده',
+                      suffixText: 'روز'),
+                ),
+                TextFormField(
+                  restorationId: 'new_days',
+                  textInputAction: TextInputAction.go,
+                  keyboardType: TextInputType.number,
+                  initialValue: _auctionNewDays.toString(),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'فاصله‌ی بین دو مزایده',
+                      suffixText: 'روز'),
+                ),
+                MaterialButton(
+                  color: Colors.blue.shade600,
+                  onPressed: () => {
+                    //todo save changes
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('تغییرات ذخیره شد'),
+                        backgroundColor: Colors.green,
+                      ),
+                    )
+                  },
+                  textColor: Colors.white,
+                  height: 50,
+                  elevation: 0,
+                  minWidth: 400,
+                  child: const Text('ذخیره تغییرات'),
+                )
+              ].expand(
+                (widget) => [
+                  widget,
+                  const SizedBox(
+                    height: 24,
+                  )
+                ],
+              )
+            ])),
+      ),
+    );
   }
 
   void showSettings(Settings settings) {

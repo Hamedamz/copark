2ee2783c2f8bab600051c0e2bd4bee65e0167432
Arshia Moghimi <arshiamoghimi98@gmail.com>
2021-07-27 06:36:00 +0430

    unRegister park place logic added

--- lib/home/info_with_reservation.dart
+++ lib/home/info_with_reservation.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 class InfoWithReservation extends StatelessWidget {
-  const InfoWithReservation({Key? key, required this.parkingNumber}) : super(key: key);
+  const InfoWithReservation({Key? key, required this.parkingNumber})
+      : super(key: key);
 
   final num parkingNumber;
+
+  Future<void> setUnreserved() async {
+    ParseCloudFunction function = ParseCloudFunction('unReserveParkPlace');
+    await function.execute();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +21,10 @@ class InfoWithReservation extends StatelessWidget {
                   const Text('شماره پارکینگ شما'),
                   Text(parkingNumber.toString()),
                   MaterialButton(
-                      onPressed: () => {
-                        //todo not using my parking today
-                      },
-                      child: const Text('امروز نمیام'),
+                    onPressed: () => {
+                    setUnreserved()
+                  },
+                    child: const Text('امروز نمیام'),
                   )
                 ]
             )
